5831be4b5444b14ec78e6611c7fd2f16b6c8af60
Hyeon Gu Kang <gusrns48@naver.com>
2021-11-21 03:10:17 +0900

    hotfix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  let [a, b] = useState(['코트','바지','모자']);
+  
+  let [zero, cnt] = useState(0);
+
+  let posts = '강남';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{ a[0] } <span onClick={ ()=>{ cnt(zero++) } }>👍</span> {zero} </h3>
+        <p>2월 7일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ a[1] }</h3>
+        <p>2월 7일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ a[2] }</h3>
+        <p>2월 7일 발행</p>
+        <hr/>
+      </div>
+
+      <Modal />
+
+
     </div>
   );
 }
+
+function Modal(){
+  return (
+    <>
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+    </>
+  )
+}
+
 
 export default App;
